3ab06689367645aef18bf2745263932d1d6763e7
Alisha Kodibagkar <46933681+alishakodibagkar@users.noreply.github.com>
2023-01-18 20:12:48 -0500

    Update config.js

--- ui-changes/src/config.js
+++ ui-changes/src/config.js
@@ -1,11 +1,11 @@
 module.exports = {
   /* eslint-disable */
-  githubSrc: 'https://raw.githubusercontent.com/alishakodibagkar/MumbleMelody_Longitudinal_Initial_Survey/master/protocol/Initial_Survey_schema',
+  githubSrc: 'https://raw.githubusercontent.com/alishakodibagkar/MumbleMelody_Longitudinal_Final_Survey/master/protocol/Final_Survey_schema',
   startButton: {
     "en": 'Join'
   },
-  banner: 'Survey for More Information: Mumble Melody Longitudinal Study',
-  assetsPublicPath: '/MumbleMelody_Longitudinal_Initial_Survey/',
+  banner: 'Final Survey: Mumble Melody Longitudinal Study',
+  assetsPublicPath: '/MumbleMelody_Longitudinal_Final_Survey/',
   contact: 'akodiba1@mit.edu',
   showHelp: true,
   checkMediaPermission: true
